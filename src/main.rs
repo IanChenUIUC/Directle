585432f0b7bb6422c113b9a7503d8063fa57a075
Benlin Gan <benlingan@gmail.com>
2024-02-22 16:30:17 -0600

    fix bug with filepath

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
 
     let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let contents = fs::read_to_string("src/hello.html").unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
