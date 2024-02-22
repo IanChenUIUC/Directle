b6a1f91c04658377aebfed7554fd9682b76b1157
Yasmercy <ianchensoftware@gmail.com>
2024-02-22 17:01:45 -0600

    word.db

--- sql_server/src/main.rs
+++ sql_server/src/main.rs
@@ -1,13 +1,30 @@
 mod word;
 
-use std::error::*;
-use std::result::*;
-use mysql::*;
-use mysql::prelude::*;
-use crate::word::*;
+
+use ormlite::sqlite::SqliteConnection;
+use ormlite::model::*;
+use word::Word;
 
 
-fn main() {
-    let word1 = Word::new("hello".to_owned(), vec![1., 2., 3.]);
-    let word2 = Word::new("world".to_owned(), vec![3., 2., 1.]);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Start by making a database connection.
+    let mut conn = <SqliteConnection as ormlite::Connection>::connect(":memory:").await.unwrap();
+
+    // You can insert the model directly.
+    let mut hello =  Word::new(1, "hello".to_owned(), vec![1., 2., 3.]).insert(&mut conn).await?;
+
+    println!("{:?}", hello);
+
+    // After modifying the object, you can update all its fields.
+    hello.embedding += "1";
+    hello.update_all_fields(&mut conn).await?;
+
+    // Query builder syntax closely follows SQL syntax, translated into chained function calls.
+    let people = Word::select()
+        .where_("id > ?").bind(50)
+        .fetch_all(&mut conn).await?;
+
+    println!("{:?}", people);
+    Ok(())
 }
--- sql_server/src/word.rs
+++ sql_server/src/word.rs
@@ -1,16 +1,19 @@
-type F = f64;
+pub type F = f64;
 
-#[derive(Debug, PartialEq, Clone)]
+use ormlite::model::*;
+#[derive(Model, Debug)]
 pub struct Word {
-    word: String,
-    embedding: Vec<F>,
+    pub id: i32,
+    pub text: String,
+    pub embedding: String,
 }
 
 impl Word {
-    pub fn new(word: String, embedding: Vec<F>) -> Self {
+    pub fn new(id: i32, text: String, embedding: Vec<F>) -> Self {
         Self {
-            word,
-            embedding
+            id,
+            text,
+            embedding: format!("{:?}", embedding),
         }
     }
 }
